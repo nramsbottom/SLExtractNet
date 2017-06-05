92b9961d549cd036387bea716340bbef11a5a135
Neil Ramsbottom <nramsbottom@users.noreply.github.com>
2017-06-05 14:12:44 +0200

    Fixed reference to original name of application.

--- SLExtractNet/Properties/AssemblyInfo.cs
+++ SLExtractNet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StarlancerAudioExtract")]
+[assembly: AssemblyTitle("SLExtractNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StarlancerAudioExtract")]
+[assembly: AssemblyProduct("SLExtractNet")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
